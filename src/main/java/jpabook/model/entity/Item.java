58a3b4f456acce3ebb77c7a6c2543802a55a7285
Mavben <evrok907@gmail.com>
2023-09-12 11:29:18 +0900

    second

---
+++ src/main/java/jpabook/model/entity/Item.java
@@ -0,0 +1,20 @@
+package jpabook.model.entity;
+abstract class Item {
+    Long id;
+    String name;
+    int price;
+}
+
+class Album extends Item {
+    String artist;
+}
+
+class Movie extends Item {
+    String director;
+    String actor;
+}
+
+class Book extends Item {
+    String author;
+    String isbn;
+}
